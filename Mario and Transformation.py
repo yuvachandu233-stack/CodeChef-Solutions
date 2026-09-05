104618c89b711b39883f6a6c37fb9223bdb8df08
Yuva Chandu <yuvachandu233@gmail.com>
2026-09-05 20:41:49 +0530

    add Mario and Transformation

---
+++ Mario and Transformation.py
@@ -0,0 +1,15 @@
+#Mario and Transformation
+t=int(input())
+for i in range(t):
+    x=int(input())
+    rem=x%3
+    if rem==0:
+         print("NORMAL")
+    elif rem==1:
+        print("HUGE")
+    else:
+        print("SMALL")
+        
+    
+     
+     
