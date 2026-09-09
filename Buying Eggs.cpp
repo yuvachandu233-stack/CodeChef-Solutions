7c888425df913a0de9d8f6a4dac7e24b5b0f7c64
Yuva Chandu <yuvachandu233@gmail.com>
2026-09-09 20:36:13 +0530

    add Buying Eggs

---
+++ Buying Eggs.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x,y,f;
+	cin>>x>>y>>f;
+	if((12*y+f)>=(12*x))
+	{
+	    cout<<12*x<<endl;
+	}
+	else{
+	    cout<<12*y+f<<endl;
+	}
+
+}
